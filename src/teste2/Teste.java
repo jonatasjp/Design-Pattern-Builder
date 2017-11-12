40073a0084e4be7054c43f474ddc750da8e42cc3
Jonatas Luis <jonatasluis.unipe@gmail.com>
2017-11-12 16:05:06 -0200

    chamadas com interfaces fluentes

--- src/teste2/Teste.java
+++ src/teste2/Teste.java
@@ -1,30 +1,31 @@
 package teste2;
 
 import implementacao2.GarcomDiretor;
-import implementacao2.PizzaBuilder;
 import implementacao2.PizzaItalianaBuilder;
 import implementacao2.PizzaMargueritaBuilder;
 import implementacao2.PizzaPortuguesaBuilder;
-import implementacao2.ProdutoPizza;
 
 public class Teste {
 	public static void main(String[] args) {
 		GarcomDiretor garcom = new GarcomDiretor();
 		
-		PizzaBuilder pizzaPortuguesaBuilder = new PizzaPortuguesaBuilder();
-		ProdutoPizza pp = garcom.definePizzaBuilder(pizzaPortuguesaBuilder).montaPizza("pequena");
-		pp.exibePizza();
+		garcom
+			.definePizzaBuilder(new PizzaPortuguesaBuilder())
+			.montaPizza("pequena")
+			.exibePizza();		
 		
 		System.out.println("-------------------------------------");
 
-		PizzaBuilder pizzaItalianaBuilder = new PizzaItalianaBuilder();
-		garcom.definePizzaBuilder(pizzaItalianaBuilder);
-		pp = garcom.montaPizza("media"); 
-		pp.exibePizza(); 
+		garcom
+			.definePizzaBuilder(new PizzaItalianaBuilder())
+			.montaPizza("media")
+			.exibePizza();
 		
 		System.out.println("-------------------------------------");
 
-		PizzaBuilder pizzaMargueritaBuilder = new PizzaMargueritaBuilder();
-		garcom.definePizzaBuilder(pizzaMargueritaBuilder).montaPizza("grande").exibePizza();
+		garcom
+			.definePizzaBuilder(new PizzaMargueritaBuilder())
+			.montaPizza("grande")
+			.exibePizza();
 	}
 }
